4b58abf09b98e9cc8c72384138d72ba5aac1671e
MarcinSchmechel <marcin.schmechel@o2.pl>
2020-09-08 20:13:34 +0200

    First commit

---
+++ src/main/java/com/game/LoseYourMoney2/LoseYourMoney2Application.java
@@ -0,0 +1,13 @@
+package com.game.LoseYourMoney2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoseYourMoney2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoseYourMoney2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/LoseYourMoney2/LoseYourMoney2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.game.LoseYourMoney2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoseYourMoney2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
